5283a09aaf57690253d22b4c4c40f3643101b9df
MarcosFernandesF <marcosrff15@gmail.com>
2023-07-10 21:32:48 -0300

    Criando componentes e encapsulamento de view
    Componentes reutilizaveis
    
    Utilizando slots e props

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -1,20 +1,27 @@
 <template>
 <!-- É necessário criar uma div, pois o vue não aceita mais de um elemento solto na página. -->
-<div>
+<div class="corpo">
     <!-- Para adicionar um texto também pode ser feito como: <h1 v-text="titulo"></h1> -->
-    <h1>{{ titulo }}</h1> 
+    <h1 class="centralizado">{{ titulo }}</h1> 
     <!-- Utilizando diretiva v-for para percorrer um array de fotos. -->
-    <ul>
-        <li v-for="foto in fotos">
-            <!-- Não é possível fazer interpolação em atributos html. É necessário utilizar diretivas: v-bind: ou apenas : -->
-            <img :src="foto.url" :alt="foto.descricao">
+    <ul class="lista-fotos">
+        <li class="lista-fotos-item" v-for="foto in fotos">
+            <meu-painel :titulo="foto.titulo">
+                <img class="imagem-responsiva" :src="foto.url" alt="foto.titulo">
+            </meu-painel>
         </li>
     </ul>
 </div>
 </template>
 
 <script>
+import Painel from './components/shared/painel/Painel'
+
 export default {
+    components: {
+        'meu-painel': Painel
+    },
+
     data() { 
         return {
             titulo: "Alurapic",
@@ -32,4 +39,25 @@ export default {
 </script>
 
 <style>
+    .corpo {
+        font-family: Helvetica, sans-serif;
+        width: 96%;
+        margin: 0 auto;
+    }
+
+    .centralizado {
+        text-align: center;
+    }
+
+    .lista-fotos {
+        list-style: none;
+    }
+
+    .lista-fotos .lista-fotos-item {
+        display: inline-block;
+    }
+
+    .imagem-responsiva {
+        width: 100%;
+    }
 </style>
