4ef69dff7c60b32c3356464cf085839bcf2b68ee
MarcosFernandesF <marcosrff15@gmail.com>
2023-07-24 19:05:05 -0300

    Aprendendo sobre gerenciamento de rotas

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -1,39 +1,27 @@
 <template>
-<!-- É necessário criar uma div, pois o vue não aceita mais de um elemento solto na página. -->
-<div class="corpo">
-    <!-- Para adicionar um texto também pode ser feito como: <h1 v-text="titulo"></h1> -->
-    <h1 class="centralizado">{{ titulo }}</h1> 
-    <!-- Utilizando diretiva v-for para percorrer um array de fotos. -->
-    <ul class="lista-fotos">
-        <li class="lista-fotos-item" v-for="foto in fotos">
-            <meu-painel :titulo="foto.titulo">
-                <img class="imagem-responsiva" :src="foto.url" alt="foto.titulo">
-            </meu-painel>
-        </li>
-    </ul>
-</div>
+    <div class="corpo">
+        <nav>
+            <ul>
+                <li v-for="route in routes">
+                    <router-link :to="route.path ? route.path : '/'"> {{ route.titulo }}</router-link>
+                </li>
+            </ul>
+        </nav>
+
+        <transition name="pagina">
+            <router-view></router-view>
+        </transition>
+    </div>
 </template>
 
 <script>
-import Painel from './components/shared/painel/Painel'
+import { routes } from './routes'
 
 export default {
-    components: {
-        'meu-painel': Painel
-    },
-
-    data() { 
+    data () {
         return {
-            titulo: "Alurapic",
-            fotos: [],
+            routes
         }
-    },
-
-    // LifeCycle Hooks: Funções que são chamadas em determinados ciclos de vida de um componente.
-    created() {
-        this.$http.get('http://localhost:3000/v1/fotos')
-            .then(res => res.json())
-            .then(fotos => this.fotos = fotos, err => console.log(err));
     }
 }
 </script>
@@ -45,19 +33,11 @@ export default {
         margin: 0 auto;
     }
 
-    .centralizado {
-        text-align: center;
+    .pagina-enter, .pagina-leave-active {
+        opacity: 0;
     }
 
-    .lista-fotos {
-        list-style: none;
-    }
-
-    .lista-fotos .lista-fotos-item {
-        display: inline-block;
-    }
-
-    .imagem-responsiva {
-        width: 100%;
+    .pagina-enter-active, .pagina-leave-active {
+        transition: opacity .4s;
     }
 </style>
--- alurapic/src/main.js
+++ alurapic/src/main.js
@@ -6,10 +6,19 @@ import App from './App.vue'
 import VueResource from 'vue-resource';
 Vue.use(VueResource);
 
+// Importando e utilizando VueRouter.
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
+// Importando as rotas.
+import { routes }  from './routes';
+const router = new VueRouter({ routes });
+
 // View Instance.
 // el: Elemento em que será renderizado um template.
 // render: Qual template será renderizado.
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
