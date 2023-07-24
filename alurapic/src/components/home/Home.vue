66bab53b20a5271ae0d06cd5dfede48572119e0d
MarcosFernandesF <marcosrff15@gmail.com>
2023-07-24 19:27:01 -0300

    Corrigindo componentes que não foram anexados

---
+++ alurapic/src/components/home/Home.vue
@@ -0,0 +1,75 @@
+<template>
+    <!-- É necessário criar uma div, pois o vue não aceita mais de um elemento solto na página. -->
+    <div>
+        <!-- Para adicionar um texto também pode ser feito como: <h1 v-text="titulo"></h1> -->
+        <h1 class="centralizado">{{ titulo }}</h1> 
+        <!-- Com v-on o vue consegue escutar o evento de javascript -->
+        <input type="search" class="filtro" v-on:input="filtro = $event.target.value" placeholder="Filtre por parte do titulo">
+        <!-- Utilizando diretiva v-for para percorrer um array de fotos. -->
+        <ul class="lista-fotos">
+            <li class="lista-fotos-item" v-for="foto in fotosComFiltro">
+                <meu-painel :titulo="foto.titulo">
+                    <imagem-responsiva :url="foto.url" :titulo="foto.titulo"></imagem-responsiva>
+                </meu-painel>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import Painel from '../shared/painel/Painel.vue'
+import ImagemResponsiva from '../shared/imagem-responsiva/ImagemResponsiva.vue'
+
+export default {
+    components: {
+        'meu-painel': Painel,
+        'imagem-responsiva': ImagemResponsiva,
+    },
+
+    data() { 
+        return {
+            titulo: "Alurapic",
+            fotos: [],
+            filtro: "",
+        }
+    },
+
+    computed: {
+        fotosComFiltro() {
+            if (this.filtro) {
+                let exp = new RegExp(this.filtro, 'i');
+                return this.fotos.filter(foto => exp.test(foto.titulo));
+            }
+            else {
+                return this.fotos;
+            }
+        }
+    },
+
+    // LifeCycle Hooks: Funções que são chamadas em determinados ciclos de vida de um componente.
+    created() {
+        this.$http.get('http://localhost:3000/v1/fotos')
+            .then(res => res.json())
+            .then(fotos => this.fotos = fotos, err => console.log(err));
+    }
+}
+</script>
+
+<style>
+    .centralizado {
+        text-align: center;
+    }
+
+    .lista-fotos {
+        list-style: none;
+    }
+
+    .lista-fotos .lista-fotos-item {
+        display: inline-block;
+    }
+
+    .filtro {
+        display: block;
+        width: 100%;
+    }
+</style>
---
+++ alurapic/src/components/shared/painel/Painel.vue
@@ -0,0 +1,63 @@
+<template>
+<!-- Como o painel será reutilizado é importante criar um componente compartilhado. -->
+    <div class="painel">
+        <!-- O v-on também pode ser interpretado por um @ -->
+        <h2 class="painel-titulo" @dblclick="visivel = !visivel">{{ titulo }}</h2>
+        <transition name="painel-fade">
+            <!-- A diretiva v-show manipula o display para none ou não do elemento. -->
+            <div class="painel-conteudo" v-show="visivel">
+                <!-- Para aplicar conteudo a mais de um slot pesquisar por named slots. -->
+                <slot></slot>
+            </div>
+        </transition>
+    </div> 
+</template>
+
+<script>
+export default {
+    props: ['titulo'],
+
+    data () {
+        return {
+            visivel: true,
+        }
+    },
+}
+</script>
+
+<!-- Scoped garante que os estilos vão ser aplicados somente à este componente. -->
+<style scoped>
+    /* estilo do painel */ 
+    .painel {
+        padding: 0 auto;
+        border: solid 2px grey;
+        display: inline-block;
+        margin: 5px;
+        box-shadow: 5px 5px 10px grey;
+        width: 200px;
+        height: 100%;
+        vertical-align: top;
+        text-align: center;
+    }
+
+    .painel .painel-titulo {
+        text-align: center;
+        border: solid 2px;
+        background: lightblue;
+        margin: 0 0 15px 0;
+        padding: 10px;
+        text-transform: uppercase;
+    }
+
+    * {
+        box-shadow: 5px 5px 5px;
+    }
+
+    .painel-fade-enter, .painel-fade-leave-active {
+        opacity: 0;
+    }
+
+    .painel-fade-enter-active, .painel-fade-leave-active {
+        transition: opacity .4s;
+    }
+</style>
